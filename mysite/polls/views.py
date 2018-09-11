2a5d0b6db8b67355476f751709df1022e0ab726a
Kel <33792313+KelDawn@users.noreply.github.com>
2018-09-11 14:22:58 -0600

    views changed
    generic views added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,32 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-from .models import Question
+from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
-    return HttpRespons(template.render(context, request))
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
-    return render(request, 'polls/detail.html', {'question': question})
-    return HttpResponse("You're looking at question %s." % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
