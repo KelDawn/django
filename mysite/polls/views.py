f3e52ba655f1e0925303efe132816bc80cc2f202
Kel <33792313+KelDawn@users.noreply.github.com>
2018-09-11 11:47:05 -0600

    404 error and template system
    shortcut and additions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 
@@ -20,6 +20,7 @@ def index(request):
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
+        return render(request, 'polls/detail.html', {'question': question})
     except Question.DoesNotExist:
         raise Http404("Question does not exist.")
     return render(request, 'polls/detail.html', {'question': question})
