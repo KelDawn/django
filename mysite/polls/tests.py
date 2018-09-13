ca7ca6e7551ee7c9907a6b4c4c1bceb1628fe480
Kel <33792313+KelDawn@users.noreply.github.com>
2018-09-12 21:27:12 -0600

    error fix
    duplicate code removed

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,9 +1,7 @@
+import datetime
 from django.test import TestCase
 
 # Create your tests here.
-import datetime
-
-from django.test import TestCase
 from django.utils import timezone
 
 from .models import Question
