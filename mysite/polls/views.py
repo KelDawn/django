3a4594e772ddfbcb1d33b6fa4105dbdc0790ad6c
Kel <33792313+KelDawn@users.noreply.github.com>
2018-09-14 09:56:58 -0600

    typo fix
    keep entering "dhango"

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from dhango.views import generic
+from django.views import generic
 from .models import Choice, Question
 
 
