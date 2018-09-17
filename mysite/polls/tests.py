f0533dd12d01f18ca92f012339eac46bdf13d8d5
Kel <33792313+KelDawn@users.noreply.github.com>
2018-09-16 20:07:05 -0600

    question tests
    checks that a question whose pub_date is in the past can be displayed, and one with a pub_date in the future is not

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -60,6 +60,10 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question.>']
         )
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
 
     def test_future_question(self):
         """
@@ -70,6 +74,10 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
 
     def test_future_question_and_past_question(self):
         """
@@ -95,4 +103,12 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+class DetailView(generic.DetailView):
+    ...
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 # was_published_recently() returns False for questions whose pub_date is in the future.
